a2b78da3a55a336ea854a50c4d9b33c58e4f146c
Troy Landers <tlanders@landersconsulting.biz>
2020-11-26 10:53:32 -0600

    first commit

---
+++ src/main/java/com/lci/apiwithjwt/ApiwithjwtApplication.java
@@ -0,0 +1,13 @@
+package com.lci.apiwithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiwithjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiwithjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lci/apiwithjwt/ApiwithjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lci.apiwithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiwithjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
